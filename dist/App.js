d2474c7e49e56648e727b0be386149d721377d67
Alex Clark <aclark@aclark.net>
2023-12-10 18:40:12 -0500

    Update

---
+++ dist/App.js
@@ -0,0 +1,6 @@
+// App.js
+import React from 'react';
+const App = () => {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, "Hello, world!"));
+};
+export default App;
