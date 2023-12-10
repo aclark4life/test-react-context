9cbc6e9b76359278e33e1fb9162818f26d897df3
Alex Clark <aclark@aclark.net>
2023-12-10 18:53:57 -0500

    Update

---
+++ src/MyComponent.js
@@ -0,0 +1,16 @@
+// MyComponent.js
+import React, { useContext } from 'react';
+import MyContext from './MyContext';
+
+const MyComponent = () => {
+  const { myData, setMyData } = useContext(MyContext);
+
+  return (
+    <div>
+      <p>Data from Context: {myData}</p>
+      <button onClick={() => setMyData('New Value')}>Change Value</button>
+    </div>
+  );
+};
+
+export default MyComponent;
