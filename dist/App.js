edaa94f887f80f69d8ce829267aca055b72802d3
Alex Clark <aclark@aclark.net>
2023-12-10 18:55:31 -0500

    Update

--- dist/App.js
+++ dist/App.js
@@ -1,7 +1,8 @@
 // App.js
 import React from 'react';
-import Context from './Context.js';
+import MyProvider from './MyProvider.js';
+import MyComponent from './MyComponent.js';
 const App = () => {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, "Hello, world!"));
+  return /*#__PURE__*/React.createElement(MyProvider, null, /*#__PURE__*/React.createElement("p", null, "Hello, world!"), /*#__PURE__*/React.createElement(MyComponent, null));
 };
 export default App;
